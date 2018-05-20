b7e1123f28f33c8e0f83e56c7f6246bc45106fd8
thiago <thiago122@gmail.com>
2018-05-20 15:55:23 -0300

    seeds

--- database/seeds/ClientsTableSeeder.php
+++ database/seeds/ClientsTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ClientsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ClientsTableSeeder::class);
+        factory(\App\Client::class, 5)->create();
     }
 }
