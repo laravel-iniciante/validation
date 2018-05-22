da28fef0d4ba863ea8bcfbc7688a24d4c0581444
thiago <thiago122@gmail.com>
2018-05-21 22:33:33 -0300

    Blade - Trabalhando com layouts

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -1,5 +1,7 @@
+@extends('layouts.layout')
+@section('content')
 
-<h3>Listagem de clientes</h3>
+    <h3>Listagem de clientes</h3>
     <br/><br/>
     <a class="btn btn-default" href="{{ route('clients.create') }}">Criar novo</a>
     <table class="table table-striped">
@@ -16,20 +18,22 @@
         </tr>
         </thead>
         <tbody>
-    @foreach($clients as $client)
-    <tr>
-        <td>{{ $client->id }}</td>
-        <td>{{ $client->name }}</td>
-        <td>{{ $client->document_number }}</td>
-        <td>{{ $client->date_birth }}</td>
-        <td>{{ $client->email }}</td>
-        <td>{{ $client->phone }}</td>
-        <td>{{ $client->sex }}</td>
-        <td>
-            <a href="{{route('clients.edit',['client' => $client->id])}}">Editar</a> |
-            <a href="{{route('clients.show',['client' => $client->id])}}">Ver</a>
-        </td>
-    </tr>
-    @endforeach
-    </tbody>
+        @foreach($clients as $client)
+            <tr>
+                <td>{{ $client->id }}</td>
+                <td>{{ $client->name }}</td>
+                <td>{{ $client->document_number }}</td>
+                <td>{{ $client->date_birth }}</td>
+                <td>{{ $client->email }}</td>
+                <td>{{ $client->phone }}</td>
+                <td>{{ $client->sex }}</td>
+                <td>
+                    <a href="{{route('clients.edit',['client' => $client->id])}}">Editar</a> |
+                    <a href="{{route('clients.show',['client' => $client->id])}}">Ver</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
     </table>
+
+@endsection
