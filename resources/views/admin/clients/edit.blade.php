f0a865bd7c4f0b39c7a042ebcc5102d1c5e01680
thiago <thiago122@gmail.com>
2018-05-27 17:32:27 -0300

    Página da edição de dados

--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -4,33 +4,33 @@
     <h3>Novo Cliente</h3>
 
     @if($errors->any())
-    <ul class="alert alert-danger">
-        @foreach($errors->all() as $error)
-             <li>{{$error}}</li>
-        @endforeach
-    </ul>
+        <ul class="alert alert-danger">
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
     @endif
 
-    <form method="POST" action="{{route('clients.store')}}">
+    <form method="POST" action="{{route('clients.update',['client' => $client->id])}}">
         {{csrf_field()}}
         <div class="form-group">
             <label>Nome</label>
-            <input class="form-control" type="text" name="name">
+            <input class="form-control" type="text" name="name" value="{{$client->name}}">
         </div>
 
         <div class="form-group">
             <label>Documet number</label>
-            <input class="form-control" type="text" name="document_number">
+            <input class="form-control" type="text" name="document_number" value="{{$client->document_number}}">
         </div>
 
         <div class="form-group">
             <label>Email</label>
-            <input class="form-control" type="email" name="email">
+            <input class="form-control" type="email" name="email" value="{{$client->email}}">
         </div>
 
         <div class="form-group">
             <label>Telefone</label>
-            <input class="form-control" type="text" name="phone">
+            <input class="form-control" type="text" name="phone" value="{{$client->phone}}">
         </div>
 
         <div class="form-group">
@@ -45,25 +45,25 @@
 
         <div class="form-group">
             <label>Data de nascimento</label>
-            <input class="form-control" type="date" name="date_birth">
+            <input class="form-control" type="date" name="date_birth" value="{{$client->date_birth}}">
         </div>
 
         <div class="radio">
-            <label><input type="radio" name="sex" value="m">Masculino</label>
+            <label><input type="radio" name="sex" value="m" {{$client->sex == 'm' ? 'checked="checked"' : ''}}>Masculino</label>
         </div>
 
         <div class="radio">
-            <label><input type="radio" name="sex" value="f">Feminino</label>
+            <label><input type="radio" name="sex" value="f" {{$client->sex == 'f' ? 'checked="checked"' : ''}}>Feminino</label>
         </div>
 
         <div class="form-group">
             <label>Deficiência física</label>
-            <input class="form-control" type="text" name="physical_disability">
+            <input class="form-control" type="text" name="physical_disability" value="{{$client->physical_disability}}">
         </div>
 
         <div class="checkbox">
-            <label></label>
-            <input class="form-control" type="checkbox" name="defaulter"> Inadinplente
+            <label><input class="form-control" type="checkbox" name="defaulter" {{$client->defaulter ? 'checked="checked"' : ''}}> Inadinplente</label>
+
         </div>
 
         <input type="submit" name="Enviar" class="btn btn-default">
