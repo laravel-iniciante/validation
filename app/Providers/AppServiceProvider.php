4ba7f5336259079654620dd676ede5cfc27ca771
thiago <thiago122@gmail.com>
2018-05-31 21:18:12 -0300

    Modelando no banco de dados tipos de cliente

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+        $platform = \Schema::getConnection()->getDoctrineSchemaManager()->getDatabasePlatform();
+        $platform->registerDoctrineTypeMapping('enum', 'string');
     }
 
     /**
